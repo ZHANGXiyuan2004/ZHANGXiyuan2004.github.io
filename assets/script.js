64306baf543c27723b6ef1a9cb400f18020e9e52
ZHANGXiyuan2004 <mail_Xiyuan_Zhang@126.com>
2026-02-13 11:07:33 +0800

    1

--- assets/script.js
+++ assets/script.js
@@ -407,7 +407,7 @@ function initAdvancements() {
     // Welcome Achievement
     setTimeout(() => {
         showAdvancement("Find A Tree And Get Some Wood", "🌳");
-    }, 1000);
+    }, 3000);
 }
 
 function showAdvancement(text, icon = '💎') {
